1ac7a40cf42439fa468f6277cb771e1450752b55
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-12-31 11:02:41 +0800

    update

--- javascript.js
+++ javascript.js
@@ -4,5 +4,22 @@ fetch('https://raw.githubusercontent.com/cenzwong/gsheet-site/main/myjson.json')
 
 
   fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTxF-DtQoIAEVRoeMU6nOdZxg-gIgkvsRqCphEZixTbSyPAoGkg_kizf01v-7VcncDUcwmZR2SM-uj1/pub?output=csv')
-  .then(response => response)
+  .then(response => response.text())
   .then(data => console.log(data));
+
+
+  fetch('https://raw.githubusercontent.com/cenzwong/gsheet-site/main/mycsv.csv')
+  .then(response => response.text())
+  .then(data => console.log(data));
+
+
+  fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTxF-DtQoIAEVRoeMU6nOdZxg-gIgkvsRqCphEZixTbSyPAoGkg_kizf01v-7VcncDUcwmZR2SM-uj1/pub?output=csv')
+.then(response => response.text()) 
+.then(csvString => {
+    //Split the csv into rows
+    const rows = csvString.split('\n');
+    for (row of rows) {
+    //Split the row into each of the comma separated values
+        console.log(row.split(","));
+    }
+});
