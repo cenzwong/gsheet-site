7594bf8bf5b9deb980c7a4689678bc48695a25ce
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-12-31 10:54:29 +0800

    updaloerd

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 fetch('https://raw.githubusercontent.com/cenzwong/gsheet-site/main/myjson.json')
   .then(response => response.json())
   .then(data => console.log(data));
+
+
+  fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTxF-DtQoIAEVRoeMU6nOdZxg-gIgkvsRqCphEZixTbSyPAoGkg_kizf01v-7VcncDUcwmZR2SM-uj1/pub?output=csv')
+  .then(response => response)
+  .then(data => console.log(data));
